afbd33a7d4a4810384a122f3c9061d0bf407fa1a
Jonny Burger <jonathanburger11@gmail.com>
2022-06-22 19:35:08 +0200

    modernize template

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,4 +1,12 @@
+// All configuration options: https://remotion.dev/docs/config
+// Each option also is available as a CLI flag: https://remotion.dev/docs/cli
+// ! The configuration file does only apply if you render via the CLI !
+
 import { Config } from 'remotion';
 
 Config.Rendering.setImageFormat('jpeg');
 Config.Output.setOverwriteOutput(true);
+
+// This template processes the whole audio file on each thread which is heavy.
+// You are safe to increase concurrency if the audio file is small or your machine strong!
+Config.Rendering.setConcurrency(1);
