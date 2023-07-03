2252961a9518b2beb406830b654c164205437451
Patric Salvisberg <patric.salvisberg@gmail.com>
2023-07-03 10:31:59 +0200

    version upgrade to 4.0.0

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,6 +1,9 @@
-// All configuration options: https://remotion.dev/docs/config
-// Each option also is available as a CLI flag: https://remotion.dev/docs/cli
-// ! The configuration file does not apply if you use the Node.js API !
+/**
+ * Note: When using the Node.JS APIs, the config file
+ * doesn't apply. Instead, pass options directly to the APIs.
+ *
+ * All configuration options: https://remotion.dev/docs/config
+ */
 
 import { Config } from '@remotion/cli/config';
 
