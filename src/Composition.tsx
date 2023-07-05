01e5539de4abfe55f5db3beb24a4b050b3bef97a
stephen@middy.com <sterv@Stephens-MacBook-Pro.local>
2023-07-05 16:47:42 -0400

    added more parameter controls

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -7,54 +7,88 @@ import {
 	delayRender,
 	Img,
 	Sequence,
+	staticFile,
 	useCurrentFrame,
 	useVideoConfig,
 } from 'remotion';
-import audioSource from './assets/audio.mp3';
-import coverImg from './assets/cover.jpg';
-import { LINE_HEIGHT, PaginatedSubtitles } from './Subtitles';
+
+import { PaginatedSubtitles } from './Subtitles';
 import { z } from 'zod';
 import { zColor } from '@remotion/zod-types';
 
 export const AudioGramSchema = z.object({
+	subtitlesFileName: z.string(),
+	audioFileName: z.string(),
+	coverImgFileName: z.string(),
 	titleText: z.string(),
 	titleColor: zColor(),
 	waveColor: zColor(),
-	transcriptionColor: zColor(),
+	audioOffsetInFrames: z.number().int().min(0),
+	subtitlesTextColor: zColor(),
+	subtitlesLinePerPage: z.number().int().min(0),
+	subtitlesLineHeight: z.number().int().min(0),
+	subtitlesZoomMeasurerSize: z.number().int().min(0),
+	onlyDisplayCurrentSentence: z.boolean(),
+	mirrorWave: z.boolean(),
+	waveLinesToDisplay: z.number().int().min(0),
+	waveFreqRangeStartIndex: z.number().int().min(0),
+	waveNumberOfSamples: z.enum(['32', '64', '128', '256', '512']),
 });
 
 type MyCompSchemaType = z.infer<typeof AudioGramSchema>;
 
-const AudioViz: React.FC<{ waveColor: string }> = ({ waveColor }) => {
+const AudioViz: React.FC<{
+	waveColor: string;
+	numberOfSamples: number;
+	freqRangeStartIndex: number;
+	waveLinesToDisplay: number;
+	mirrorWave: boolean;
+	audioSrc: string;
+}> = ({
+	waveColor,
+	numberOfSamples,
+	freqRangeStartIndex,
+	waveLinesToDisplay,
+	mirrorWave,
+	audioSrc
+}) => {
 	const frame = useCurrentFrame();
 	const { fps } = useVideoConfig();
-	const audioData = useAudioData(audioSource);
+	
+	const audioData = useAudioData(audioSrc);
 
 	if (!audioData) {
 		return null;
 	}
 
-	const allVisualizationValues = visualizeAudio({
+	const frequencyData = visualizeAudio({
 		fps,
 		frame,
 		audioData,
-		numberOfSamples: 256, // Use more samples to get a nicer visualisation
+		numberOfSamples, // Use more samples to get a nicer visualisation
 	});
 
 	// Pick the low values because they look nicer than high values
 	// feel free to play around :)
-	const visualization = allVisualizationValues.slice(7, 30);
+	const frequencyDataSubset = frequencyData.slice(
+		freqRangeStartIndex,
+		freqRangeStartIndex + (mirrorWave? Math.round(waveLinesToDisplay / 2) : waveLinesToDisplay)
+	);
 
-	const mirrored = [...visualization.slice(1).reverse(), ...visualization];
+	const frequencesToDisplay = mirrorWave? [
+		...frequencyDataSubset.slice(1).reverse(),
+		...frequencyDataSubset,
+	] : frequencyDataSubset;
 
 	return (
 		<div className="audio-viz">
-			{mirrored.map((v, i) => {
+			{frequencesToDisplay.map((v, i) => {
 				return (
 					<div
 						key={i}
 						className="bar"
 						style={{
+							minWidth: "1px",
 							backgroundColor: waveColor,
 							height: `${500 * Math.sqrt(v)}%`,
 						}}
@@ -67,16 +101,27 @@ const AudioViz: React.FC<{ waveColor: string }> = ({ waveColor }) => {
 
 export const AudiogramComposition: React.FC<
 	{
-		source: string;
+		subtitlesFileName: string;
 		audioOffsetInFrames: number;
+		onlyDisplayCurrentSentence: boolean;
 	} & MyCompSchemaType
 > = ({
-	source,
+	subtitlesFileName,
+	audioFileName,
+	coverImgFileName,
 	audioOffsetInFrames,
 	titleText,
 	titleColor,
-	transcriptionColor,
+	subtitlesTextColor,
+	subtitlesLinePerPage,
 	waveColor,
+	waveNumberOfSamples,
+	waveFreqRangeStartIndex,
+	waveLinesToDisplay,
+	subtitlesZoomMeasurerSize,
+	subtitlesLineHeight,
+	onlyDisplayCurrentSentence,
+	mirrorWave,
 }) => {
 	const { durationInFrames } = useVideoConfig();
 
@@ -84,8 +129,13 @@ export const AudiogramComposition: React.FC<
 	const [subtitles, setSubtitles] = useState<string | null>(null);
 	const ref = useRef<HTMLDivElement>(null);
 
+	// get the static files
+	const subtitlesSrc = staticFile(subtitlesFileName);
+	const audioSrc = staticFile(audioFileName)
+	const coverImgSrc = staticFile(coverImgFileName);
+
 	useEffect(() => {
-		fetch(source)
+		fetch(subtitlesSrc)
 			.then((res) => res.text())
 			.then((text) => {
 				setSubtitles(text);
@@ -94,17 +144,19 @@ export const AudiogramComposition: React.FC<
 			.catch((err) => {
 				console.log('Error fetching subtitles', err);
 			});
-	}, [handle, source]);
+	}, [handle, subtitlesSrc]);
 
 	if (!subtitles) {
 		return null;
 	}
 
+
+
 	return (
 		<div ref={ref}>
 			<AbsoluteFill>
 				<Sequence from={-audioOffsetInFrames}>
-					<Audio src={audioSource} />
+					<Audio src={audioSrc} />
 
 					<div
 						className="container"
@@ -113,7 +165,7 @@ export const AudiogramComposition: React.FC<
 						}}
 					>
 						<div className="row">
-							<Img className="cover" src={coverImg} />
+							<Img className="cover" src={coverImgSrc} />
 
 							<div className="title" style={{ color: titleColor }}>
 								{titleText}
@@ -121,19 +173,29 @@ export const AudiogramComposition: React.FC<
 						</div>
 
 						<div>
-							<AudioViz waveColor={waveColor} />
+							<AudioViz
+								audioSrc={audioSrc}
+								mirrorWave={mirrorWave}
+								waveColor={waveColor}
+								numberOfSamples={Number(waveNumberOfSamples)}
+								freqRangeStartIndex={waveFreqRangeStartIndex}
+								waveLinesToDisplay={waveLinesToDisplay}
+							/>
 						</div>
 
 						<div
-							style={{ lineHeight: `${LINE_HEIGHT}px` }}
+							style={{ lineHeight: `${subtitlesLineHeight}px` }}
 							className="captions"
 						>
 							<PaginatedSubtitles
 								subtitles={subtitles}
 								startFrame={audioOffsetInFrames}
 								endFrame={audioOffsetInFrames + durationInFrames}
-								linesPerPage={4}
-								transcriptionColor={transcriptionColor}
+								linesPerPage={subtitlesLinePerPage}
+								subtitlesTextColor={subtitlesTextColor}
+								subtitlesZoomMeasurerSize={subtitlesZoomMeasurerSize}
+								subtitlesLineHeight={subtitlesLineHeight}
+								onlyDisplayCurrentSentence={onlyDisplayCurrentSentence}
 							/>
 						</div>
 					</div>
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,4 +1,3 @@
-import { staticFile } from 'remotion';
 import { Composition } from 'remotion';
 import { AudioGramSchema, AudiogramComposition } from './Composition';
 import './style.css';
@@ -18,13 +17,31 @@ export const RemotionRoot: React.FC = () => {
 				height={1080}
 				schema={AudioGramSchema}
 				defaultProps={{
+
+					// subtitles settings
+					subtitlesFileName: 'subtitles.srt',
+
+					// title settings
+					audioFileName: 'audio.mp3',
+					coverImgFileName: 'cover.jpg',
 					titleText:
 						'#234 – Money, Kids, and Choosing Your Market with Justin Jackson of Transistor.fm',
-					titleColor: 'rgba(251, 176, 14, 0.93)',
-					waveColor: '#ffae00',
-					transcriptionColor: 'rgba(255, 255, 255, 0.93)',
+					titleColor: 'rgba(186, 186, 186, 0.93)',
+
+					// subtitles settings
+					onlyDisplayCurrentSentence: true,
+					subtitlesTextColor: 'rgba(255, 255, 255, 0.93)',
+					subtitlesLinePerPage: 4,
+					subtitlesZoomMeasurerSize: 10,
+					subtitlesLineHeight: 98,
 					audioOffsetInFrames: 207,
-					source: staticFile('subtitles.srt'),
+					
+					// wave settings
+					waveColor: '#a3a5ae',
+					waveFreqRangeStartIndex: 7,
+					waveLinesToDisplay: 29,
+					waveNumberOfSamples: "256", // this is string for Remotion controls and will be converted to a number
+					mirrorWave: true					
 				}}
 			/>
 		</>
