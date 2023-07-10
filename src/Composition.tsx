c83736865c57ab25050951be2a9f963f2d596a64
Jonny Burger <jonathanburger11@gmail.com>
2023-07-10 11:07:54 +0200

    must have right extension

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -20,9 +20,24 @@ import { zColor } from '@remotion/zod-types';
 export const AudioGramSchema = z.object({
 	durationInSeconds: z.number().positive(),
 	audioOffsetInSeconds: z.number().min(0),
-	subtitlesFileName: z.string(),
-	audioFileName: z.string(),
-	coverImgFileName: z.string(),
+	subtitlesFileName: z.string().refine((s) => s.endsWith('.srt'), {
+		message: 'Subtitles file must be a .srt file',
+	}),
+	audioFileName: z.string().refine((s) => s.endsWith('.mp3'), {
+		message: 'Subtitles file must be a .mp3 file',
+	}),
+	coverImgFileName: z
+		.string()
+		.refine(
+			(s) =>
+				s.endsWith('.jpg') ||
+				s.endsWith('.jpeg') ||
+				s.endsWith('.png') ||
+				s.endsWith('.bmp'),
+			{
+				message: 'Subtitles file must be a .jpg / .jpeg / .png / .bmp file',
+			}
+		),
 	titleText: z.string(),
 	titleColor: zColor(),
 	waveColor: zColor(),
